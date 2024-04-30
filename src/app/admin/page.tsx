7114be30dbc79f8b779b0cb3bfb05400594ab8d2
Mohamad Khaefi <mkhaefi2500@yahoo.com>
2024-04-30 16:23:30 +0330

    Finish Fetching user - product - orders data in admin/page / Add loading for admin/page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,58 +1,114 @@
-import React from "react"
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import db from "@/db/db";
-import {formatCurrency, formatNumber} from "@/lib/formatters";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
 
+// Get Sales Data
 async function getSalesData() {
-    const data = await db?.order.aggregate({
-        _sum: {pricePaidInCents: true},
-        _count: true
-    })
-    return {
-        amount: (data?._sum.pricePaidInCents || 0) / 100,
-        numberOfSales: data?._count,
-    }
-
+  const data = await db?.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+  return {
+    amount: (data?._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data?._count,
+  };
 }
 
+// Get user Data
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { pricePaidInCents: true },
+    }),
+  ]);
+  await wait(2000);
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  };
+}
+
+// Get Product Data
+async function getProductData() {
+  const [activeCount, inactiveCounts] = await Promise.all([
+    db.product.count({
+      where: {
+        isAvailableForPurchase: true,
+      },
+    }),
+    db.product.count({
+      where: {
+        isAvailableForPurchase: false,
+      },
+    }),
+  ]);
+  return { activeCount, inactiveCounts };
+}
+function wait(duration: number, title: string) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+}
 
 const AdminDashboardPage = async () => {
-    const salesData = await getSalesData();
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ]);
 
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <DashboardCard title="Sales" subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
-                           body={formatCurrency(salesData.amount)}/>
-
-            <DashboardCard title="Sales" subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
-                           body={formatCurrency(salesData.amount)}/>
-
-
-        </div>
-    )
-}
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard
+        title="Sales"
+        subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
+        body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title="Customers"
+        subtitle={`${formatCurrency(
+          userData.averageValuePerUser,
+        )} Average Value`}
+        body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active Products"
+        subtitle={`${formatNumber(productData.inactiveCounts)} Inactive`}
+        body={formatNumber(productData.activeCount)}
+      />
+    </div>
+  );
+};
 
 export default AdminDashboardPage;
 
 type DashboardCardProps = {
-    title: string
-    subtitle: string
-    body: string
-}
+  title: string;
+  subtitle: string;
+  body: string;
+};
 
-function DashboardCard({title, subtitle, body}: DashboardCardProps) {
-    return (
-        <Card>
-            <CardHeader>
-                <CardTitle>{title}</CardTitle>
-                <CardDescription>
-                    {subtitle}
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <p>{body}</p>
-            </CardContent>
-        </Card>
-
-    )
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
 }
